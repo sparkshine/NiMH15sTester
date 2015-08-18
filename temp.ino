38d1b355adbe92192878fb71cd25aef25a2a6c48
Philip Ozorio <p.ozorio@sparkshine.net>
2015-08-18 16:03:19 +0100

    0.1-b1
    Version Beta 1
    -------------
    Implemented the remaining functions.
    
    Known issues
    -Temperature is slightly off (±2°C).
    -ADC voltage is only accurate when vcc value is correct. vcc changes
    sometimes.

--- temp.ino
+++ temp.ino
@@ -17,32 +17,32 @@ void displaySerial();
 const int minusButton = 8;     // the number of the pushbutton pin
 const int plusButton = 7;     // the number of the pushbutton pin
 const int okButton = 9;     // the number of the pushbutton pin
-const int voltagePin = A2;  //Voltage measurement pin
-const int currentPin = A3;  //Voltage measurement pin
 /*-----( Declare Variables )-----*/
 byte day;
 byte month;
 byte year;
 int serialNo;
 int lastSerialNo;
-int resultV = 100;
-int resultI = 100;
+int discard;
 int temperature;
 int packR;
+uint8_t count = 0;
 uint8_t fail = 0;
-float vcc = 5.03;
+float vcc = 4.972;
 byte minusButtonLast = 1;     // the number of the pushbutton pin
 byte plusButtonLast = 1;     // the number of the pushbutton pin
 //int okButtonLast = 0;     // the number of the pushbutton pin
 /*-----( Declare objects )-----*/
 File SDFileData;
-#define SD_CARD_CD_DIO 10 /* DIO pin used to control the modules CS pin */
-#define SEMITEC_103JT_10k 3435.0f,298.15f,10000.0f  // B,T0,R0
+#define voltagePin A3 // Pack output (Common V output)
+
+#define SD_CARD_CD_DIO 10 // DIO pin used to control the modules CS pin
+#define SEMITEC_103JT_10k 3435.0f,293.15f,12110.0f  // THERMISTOR USED. BETA,T0,R0
 #define THERM1PIN 0     // Arduino Thermistor PIN
-#define THERM2PIN 1     // Pack Thermistor PIN            
-#define MOSFET 6 //
-#define R2_Balance 11937.0f //
-#define R1_Balance 9901.0f //
+#define THERM2PIN 2     // Pack Thermistor PIN            
+#define MOSFET 6 // MOSFET pin for short circut
+#define R2_Balance 11937.0f // Divider for system side thermistor
+#define R1_Balance 9901.0f // Divider for pack side thermistor
 // set the LCD address to 0x27 for a 20 chars 4 line display
 // Set the pins on the I2C chip used for LCD connections:
 // addr, en,rw,rs,d4,d5,d6,d7,bl,blpol
@@ -54,69 +54,55 @@ enum { // enumerating 3 major temperature scales
   T_FAHRENHEIT
 };
 
-/*----( SETUP: RUNS ONCE )----*/
-void setup() {
+void setup() { /*----( SETUP: RUNS ONCE )----*/
   uint32_t currentFrequency;
   Serial.begin(9600);  // Used to type in characters
-  ina219.begin();
-  lcd.begin(16, 2);  // initialize the lcd for 16 chars 2 lines, turn on backlight
-  /* DIO pin uesd for the CS function. Note that even if you are not driving this
-   function from your Arduino board, you must still configure this as an output
-   otherwise the SD library functions will not work. */
-  pinMode(10, OUTPUT);
+  ina219.begin(); // Current sensor
+  lcd.begin(16, 2);  // Initialize the lcd for 16 chars 2 lines
   lcd.backlight(); // backlight on
-
+  pinMode(SD_CARD_CD_DIO, OUTPUT); //CS PIN for SD card
   if (!SD.begin(SD_CARD_CD_DIO)) {
     lcd.clear();
     lcd.setCursor(0, 0); //Start at character 4 on line 0
     lcd.print("SD ERROR");
     while (1);
-  } else{
+  } else {
     lcd.clear();
   }
   pinMode(minusButton, INPUT);
   pinMode(plusButton, INPUT);
   pinMode(okButton, INPUT);
-
+  pinMode(MOSFET, OUTPUT);
 }/*--(end setup )---*/
 
 
-void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
-{
+void loop() {  /*----( LOOP: RUNS CONSTANTLY )----*/
   setDate();
   confirmDate();
   writeDate();
   findLastSerial();
-  Serial.println("found last serial");
   while (1) {
     readingValues();
   }
-
 }/* --(end main loop )-- */
-
 
 /*----( FUNCTIONS )----*/
 /////////////////////////
 
 /*---------------( USER SETTING OF SDCARD DATE )-------------*/
-void setDate(){
+void setDate() {
   lcd.clear();
   /* Check if the text file exists */
-  if (SD.exists("date.txt"))
-  {
-    Serial.println("date.txt exists, attempting to read file...");
-
+  if (SD.exists("date.txt")) {
+//    Serial.println("date.txt exists, attempting to read file...");
     /* The file exists so open it */
     SDFileData = SD.open("date.txt");
-
     day = SDFileData.parseInt();
     month = SDFileData.parseInt();
     year = SDFileData.parseInt();
-
     SDFileData.close();
   }
-  else
-  {
+  else {
     lcd.clear();
     lcd.setCursor(0, 0); //Start at character 4 on line 0
     lcd.print("SD-ERROR");
@@ -124,10 +110,8 @@ void setDate(){
     lcd.print("DATE-LOST");
     while (1);
   }
-
   while (digitalRead(okButton) == 1) {
-    if (digitalRead(minusButton) != minusButtonLast)
-    {
+    if (digitalRead(minusButton) != minusButtonLast) {
       delay(200);
       minusButtonLast != minusButtonLast;
       day++;
@@ -135,8 +119,7 @@ void setDate(){
         day = 1;
       }
     }
-    if (digitalRead(plusButton) != plusButtonLast)
-    {
+    if (digitalRead(plusButton) != plusButtonLast) {
       delay(200);
       plusButtonLast != plusButtonLast;
       month++;
@@ -146,12 +129,10 @@ void setDate(){
     }
     displayDate();
   }
-
   // Confirm Date
   lcd.setCursor(0, 1); //Start at character 4 on line 0
   lcd.print("SET? CANCEL? ---");
 }
-
 /*---------------( CONFIRM DATE )-------------*/
 void confirmDate()
 {
@@ -172,43 +153,33 @@ void confirmDate()
 }
 
 /*---------------( DISPLAY DATE TO LCD SCREEN )-------------*/
-void displayDate()
-{
+void displayDate() {
   //Date
-  //lcd.clear();
   lcd.setCursor(0, 0); //Start at character 4 on line 0
   lcd.print("DATE:");
-
   //Day
   lcd.setCursor(7, 0); //Start at character 4 on line 0
   char buff[5];
   sprintf(buff, "%02d", day);
   lcd.print(buff);
-
-
   //Month
   lcd.setCursor(10, 0); //Start at character 4 on line 0
   sprintf(buff, "%02d", month);
   lcd.print(buff);
-
   //Year
   lcd.setCursor(13, 0); //Start at character 4 on line 0
   sprintf(buff, "%02d", year);
   lcd.print(buff);
 
-
   lcd.setCursor(0, 1);
   lcd.print("DD++  MM++   OK");
-  //delay(1000);
-
 }
 
 /*---------------( WRITE DATE TO SDCARD )-------------*/
 void writeDate() {
   SDFileData = SD.open("date.txt", FILE_WRITE);
   /* Check if the text file already exists */
-  while (SD.exists("date.txt"))
-  {
+  while (SD.exists("date.txt")) {
     /* If so then delete it */
     //Serial.println("date.txt already exists...DELETING");
     SD.remove("date.txt");
@@ -230,32 +201,25 @@ void writeDate() {
   }
 }
 
-void findLastSerial()
-{
+void findLastSerial() {
   //Serial.println("find last serial");
-
   /* Check if the text file exists */
-  if (SD.exists("results.txt")) {
-    //Serial.println("results.txt exists, attempting to read file...");
-    /* The file exists so open it */
+  if (SD.exists("results.txt")) { /* The file exists so open it */
     SDFileData = SD.open("results.txt");
     while (SDFileData.available()) {
-      //      lastSerialNo = SDFileData.parseInt();
-      //      serialNo = lastSerialNo + 1;
-      //      resultV = SDFileData.parseInt();
-      //      //resultI = SDFileData.parseInt();
-      //      SDFileData.parseInt();
       lastSerialNo = SDFileData.parseInt();
       serialNo = lastSerialNo + 1;
-      resultV = SDFileData.parseInt();
-      Serial.print("S/N: ");
-      Serial.print(serialNo);
-      Serial.print(" ");
-      byte lastDay = SDFileData.parseInt();
-      byte lastMonth = SDFileData.parseInt();
-      byte lastYear = SDFileData.parseInt();
+      SDFileData.parseInt();
+      SDFileData.parseInt();
+      SDFileData.parseFloat();
+      SDFileData.parseFloat();
+      SDFileData.parseFloat();
+//      Serial.print("S/N: ");
+//      Serial.println(serialNo);
+      SDFileData.parseInt();
+      SDFileData.parseInt();
+      SDFileData.parseInt();
     }
-
     SDFileData.close();
     lcd.clear();
     lcd.print("Last S/N: ");
@@ -286,9 +250,10 @@ void findLastSerial()
     lcd.print("RESULTS LOST");
     while (1);
   }
-  Serial.println("display serial");
+//  Serial.println("display serial");
+  lcd.clear();
   displaySerial();
-  Serial.println("return to main");
+//  Serial.println("return to main");
 }
 
 float Temperature(int AnalogInputNumber, int OutputUnit, float B, float T0, float R0, float R_Balance) {
@@ -306,7 +271,7 @@ float Temperature(int AnalogInputNumber, int OutputUnit, float B, float T0, floa
     default:
       break;
   };
-  Serial.print(T);
+  Serial.print("Temp: "); Serial.print(T); Serial.println("C");
   return T;
 }
 
@@ -317,15 +282,11 @@ int resistanceErr() {
   lcd.print("RETEST-- CONT++");
   while (1) {
     if (digitalRead(minusButton) != minusButtonLast) {
-      delay(200);
-      packR = readResistance();
-      checkResistance(temperature, packR);
-      if (bitRead(fail, 0) != 0) {
-        break;
-      }
+      count++;
+      break;
     }
     if (digitalRead(plusButton) != plusButtonLast) {
-      delay(200);
+      delay(500);
       minusButtonLast != minusButtonLast;
       bitSet(fail, 0);
       break;
@@ -336,6 +297,7 @@ int resistanceErr() {
 int readResistance() {
   float rawADC = analogRead(THERM2PIN);
   int packR = rawADC * R2_Balance / (1024.0f - rawADC);
+  Serial.print("Resistance: "); Serial.print(packR); Serial.println(" ohms");
   return packR;
 }
 
@@ -344,58 +306,68 @@ int checkResistance(int temperature, int packR) {
     if ( packR < 13000 || packR > 16500) {
       resistanceErr();
     }
+    else {
+      bitSet(fail, 0);
+    }
   }
   if (temperature > 17.5 && temperature <= 20) {
     if ( packR < 11500 || packR > 15000) {
       resistanceErr();
+    }
+    else {
+      bitSet(fail, 0);
     }
   }
   if (temperature > 20 && temperature <= 22.5) {
     if ( packR < 10000 || packR > 13500) {
       resistanceErr();
     }
+    else {
+      bitSet(fail, 0);
+    }
   }
   if (temperature > 22.5 && temperature <= 25) {
     if ( packR < 9000 || packR > 12000) {
       resistanceErr();
+    }
+    else {
+      bitSet(fail, 0);
     }
   }
   if (temperature > 25 && temperature <= 27.5) {
     if ( packR < 8000 || packR > 11000) {
       resistanceErr();
     }
+    else {
+      bitSet(fail, 0);
+    }
   }
   if (temperature > 27.5 && temperature <= 30) {
     if ( packR < 7000 || packR > 10000) {
       resistanceErr();
     }
+    else {
+      bitSet(fail, 0);
+    }
+    if( temperature > 30 || temperature < 15) {
+      lcd.clear();
+      lcd.print("TEMP PROBLEM");
+      resistanceErr();
+      bitSet(fail,0);
+    }
   }
-  bitSet(fail, 0);
 }
-
 
 /*---------------( ENTER SERIAL NUMBER, READING VALUES )-------------*/
 void readingValues() {
-  freeRAM();
   float shuntvoltage = 0;
   float busvoltage = 0;
   float current_mA = 0;
   float loadvoltage = 0;
-
-  shuntvoltage = ina219.getShuntVoltage_mV();
-  busvoltage = ina219.getBusVoltage_V();
-  current_mA = ina219.getCurrent_mA();
-  loadvoltage = busvoltage + (shuntvoltage / 1000);
-  
-  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
-  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
-  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
-  Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
-  Serial.println("");
   lcd.clear();
   float voltage = analogRead(voltagePin);
-  lcd.print(voltage);
-  while (voltage < 10) {
+
+  while (voltage < 620 || voltage > 939) { // Check pack voltage between 15V and 22.5V
     if (digitalRead(minusButton) != minusButtonLast) {
       delay(200);
       if (serialNo > lastSerialNo + 1) {
@@ -411,40 +383,75 @@ void readingValues() {
     displaySerial();
     voltage = analogRead(voltagePin);
   }
-  temperature = Temperature(THERM1PIN, T_CELSIUS, SEMITEC_103JT_10k, R1_Balance);
-  packR = readResistance();
-  while ( bitRead(fail, 0) != 1) {
-    checkResistance(temperature, packR);
-    bitRead(fail, 0);
-  }
-
-  Serial.print("PACK P:");
-  Serial.println(packR);
-  //Once pack has been connected leave for delay until voltage and current settles
   lcd.clear();
   lcd.setCursor(0, 0); //Start at character 4 on line 0
-  lcd.print("READING VALUES..");
-  delay(500);
+  lcd.print("TESTING VOLTAGE");
+  //  lcd.print(serialNo);
   //delay to allow voltage to settle after connecting pack
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < 6; i++) {
     lcd.setCursor(i, 1); //Start at character 4 on line 0
     lcd.print(">");
-    delay(124);//100ms delay
+    delay(160);//100ms delay
   }
-  // Show Pack has been read and serial assigned
+  shuntvoltage = ina219.getShuntVoltage_mV();
+  busvoltage = ina219.getBusVoltage_V();
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
+  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
+  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
+  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
+
+  voltage = ((voltage * vcc / 1024.0f) * (16.5f + 4.22f)) / 4.22f; // convert ADC to actual voltage
+  Serial.print("ADC Voltage: "); Serial.print(voltage); Serial.println(" V");
+  while (bitRead(fail, 0) != 1 ) {
+    temperature = Temperature(THERM1PIN, T_CELSIUS, SEMITEC_103JT_10k, R1_Balance);
+    packR = readResistance();
+    checkResistance(temperature, packR);
+    delay(500);
+    bitRead(fail, 0);
+    if (count > 5) { //UP TO 5
+      break;
+    }
+  }
+  count = 0; //RESET ERROR COUNTER
+  lcd.setCursor(0, 0);
   lcd.clear();
-  lcd.print("SN:");
-  lcd.setCursor(3, 1); //Start at character 4 on line 0
-  lcd.print(serialNo);
-  delay(1000);
-  lcd.setCursor(10, 1); //Start at character 4 on line 0
-  lcd.print("CANCEL:");
-  while (analogRead(voltagePin) < 10) {
+  lcd.print("TESTING CURRENT");
+  digitalWrite(MOSFET, HIGH);
+  lcd.setCursor(0, 1);
+  lcd.print(">>>>>>");
+  for (int i = 6; i < 12; i++) {
+    lcd.setCursor(i, 1);
+    lcd.print(">");
+    delay(1666);// Wait for 10 seconds
+  }
+
+  current_mA = ina219.getCurrent_mA();
+  Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
+  Serial.println("");
+  delay(200);
+  digitalWrite(MOSFET, LOW);
+  delay(150);
+  lcd.clear();
+  if (analogRead(voltagePin) < 620 || analogRead(voltagePin) > 939) {
+    lcd.print("VOLTAGE ERROR");
+    lcd.setCursor(10, 1); //Start at character 4 on line 0
+    lcd.print("CANCEL");
+    while (1) {
+      if (digitalRead(okButton) == 0) {
+        return;
+      }
+      delay(100);
+    }
+  }
+  while (analogRead(voltagePin) > 620) {
+    lcd.print("REMOVE PACK");
+    lcd.setCursor(10, 1); //Start at character 4 on line 0
+    lcd.print("CANCEL");
     if (digitalRead(okButton) == 0) {
       return;
     }
+    delay(100);
   }
-  //saveResults();
   SDFileData = SD.open("results.txt", FILE_WRITE);
   if (SDFileData) {
     SDFileData.println("");
@@ -454,12 +461,12 @@ void readingValues() {
     SDFileData.print(",");
     SDFileData.print(packR);
     SDFileData.print(",");
-    //    SDFileData.print(voltage1);
-    //    SDFileData.print(",");
-    //    SDFileData.print(voltage2);
-    //    SDFileData.print(",");
-    //    SDFileData.print(scCurrent);
-    //    SDFileData.print(",");
+    SDFileData.print(voltage);
+    SDFileData.print(",");
+    SDFileData.print(loadvoltage);
+    SDFileData.print(",");
+    SDFileData.print(current_mA);
+    SDFileData.print(",");
     SDFileData.print(day);
     SDFileData.print("/");
     SDFileData.print(month);
@@ -475,37 +482,25 @@ void readingValues() {
   lcd.clear();
 }
 
-void shortcircuitI()
-{
-  digitalWrite(MOSFET, HIGH);
-  delay(10000);
-  //take current reading
-}
-
 void displaySerial() {
-  //lcd.clear();
   lcd.setCursor(0, 0); //Start at character 4 on line 0
   lcd.print("INSERT PACK");
   lcd.setCursor(0, 1); //Start at character 4 on line 0
-  lcd.print("SN:");
-
-  //char buff[5];
-  //sprintf(buff, "%05d", serialNo);
-  //lcd.print(buff);
-
-  lcd.setCursor(3, 1); //Start at character 4 on line 0
+  lcd.print("S/N:");
+  lcd.setCursor(4, 1); //Start at character 4 on line 0
   lcd.print(serialNo);
-
+  delay(200);
 }
 
 
-static void freeRAM () {
-  extern int __heap_start, *__brkval;
-  int v;
-  int free = (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
-  Serial.print(F("Free RAM : "));
-  Serial.println(free); // Which is the unit?
-}
+/* FUNCTION THAT CHECKS FREE MEMORY AVAILABLE */
+//static void freeRAM () {
+//  extern int __heap_start, *__brkval;
+//  int v;
+//  int free = (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
+//  Serial.print(F("Free RAM : "));
+//  Serial.println(free); // Which is the unit?
+//}
 
 
 
